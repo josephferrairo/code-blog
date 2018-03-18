d1d549feb5ddab08ee0b05d366bc9832a0d76eae
Joseph Ferrairo <josephferrairo@gmail.com>
2018-03-18 17:56:08 -0400

    Added the full_name method for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
   validates :first_name, :last_name, presence: true
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,27 +1,37 @@
 require 'rails_helper'
 
 describe User do
-  it 'has many posts' do
-    user = FactoryBot.create(:user)
-    FactoryBot.create(:post, user: user)
-    FactoryBot.create(:post, user: user)
+  describe 'User attributes / validations' do
+    it 'has many posts' do
+      user = FactoryBot.create(:user)
+      FactoryBot.create(:post, user: user)
+      FactoryBot.create(:post, user: user)
 
-    expect(user.posts.count).to eq(2)
-  end
-  it 'creates a valid user' do
-    user = User.new(first_name: 'John',
-                    last_name: 'Smith',
-                    email: 'kanye@kanye.gov',
-                    password: 'password')
-    expect(user).to be_valid
-  end
+      expect(user.posts.count).to eq(2)
+    end
+    it 'creates a valid user' do
+      user = User.new(first_name: 'John',
+                      last_name: 'Smith',
+                      email: 'kanye@kanye.gov',
+                      password: 'password')
+      expect(user).to be_valid
+    end
 
-  it 'validates the presence of first_name and last name' do
-    user = User.new(email: 'kanye@kanye.gov',
-                    password: 'password')
+    it 'validates the presence of first_name and last name' do
+      user = User.new(email: 'kanye@kanye.gov',
+                      password: 'password')
 
-    expect(user).not_to be_valid
-    expect(user.errors.messages[:first_name]).to eq(["can't be blank"])
-    expect(user.errors.messages[:last_name]).to eq(["can't be blank"])
+      expect(user).not_to be_valid
+      expect(user.errors.messages[:first_name]).to eq(["can't be blank"])
+      expect(user.errors.messages[:last_name]).to eq(["can't be blank"])
+    end
+
+    describe 'full_name' do
+      it 'will return the full name' do
+        user = FactoryBot.create(:user, first_name: 'Joe', last_name: 'Smith')
+        expect(user.full_name).to eq('Joe Smith')
+      end
+    end
+
   end
 end
