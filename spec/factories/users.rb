f081bc5373a9326fdd028eee2d4fc08225320e8e
Joseph Ferrairo <josephferrairo@gmail.com>
2018-03-18 17:45:00 -0400

    Added a User Profiles page(show + index)
    - Added first_name and last_name attributes to User
    - Added the users/registration_controller to override the default devise
    behavior. We need this in order to pass our new attributes as params

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
+    sequence(:first_name) { |n| "#{n} Mike"}
+    sequence(:last_name) { |n| "#{n} Smith"}
     sequence(:email) { |n| "#{n}@#{n}.com" }
     password "password"
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,27 @@
 require 'rails_helper'
 
 describe User do
-  it 'A user has many posts' do
+  it 'has many posts' do
     user = FactoryBot.create(:user)
     FactoryBot.create(:post, user: user)
     FactoryBot.create(:post, user: user)
 
     expect(user.posts.count).to eq(2)
+  end
+  it 'creates a valid user' do
+    user = User.new(first_name: 'John',
+                    last_name: 'Smith',
+                    email: 'kanye@kanye.gov',
+                    password: 'password')
+    expect(user).to be_valid
+  end
+
+  it 'validates the presence of first_name and last name' do
+    user = User.new(email: 'kanye@kanye.gov',
+                    password: 'password')
+
+    expect(user).not_to be_valid
+    expect(user.errors.messages[:first_name]).to eq(["can't be blank"])
+    expect(user.errors.messages[:last_name]).to eq(["can't be blank"])
   end
 end
