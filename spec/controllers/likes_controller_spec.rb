0e55dc8922d109e689f10f165c22a1ffdadc74d5
Joseph Ferrairo <josephferrairo@gmail.com>
2018-03-27 22:14:18 -0400

    Add not logged in tests to the likes controller spec

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -9,13 +9,26 @@ RSpec.describe LikesController, type: :controller do
       login_user(user)
       expect(Like.count).to eq(0)
 
-      like_params = {  post_id: post1.id, user_id: user.id }
+      like_params = { post_id: post1.id, user_id: user.id }
       post :create, params: like_params
 
       like = Like.last
       expect(Like.count).to eq(1)
       expect(like.user).to eq(user)
       expect(like.post).to eq(post1)
+    end
+
+    it 'must be logged in' do
+      post1 = FactoryBot.create(:post)
+      user = FactoryBot.create(:user)
+
+      expect(Like.count).to eq(0)
+
+      like_params = { post_id: post1.id, user_id: user.id }
+      post :create, params: like_params
+
+      assert_redirected_to new_user_session_path
+      expect(Like.count).to eq(0)
     end
   end
 
@@ -37,10 +50,24 @@ RSpec.describe LikesController, type: :controller do
       login_user(user)
       post1 = FactoryBot.create(:post)
       like = FactoryBot.create(:like, user: other_user, post: post1)
+      like_params =  { post_id: post1.id, user: user.id, id: like.id}
+
+      expect(Like.count).to eq(1)
+      expect { delete :destroy, params: like_params }.to change(Like, :count).by(0)
+      expect(flash[:alert]).to eq('That is not your like to delete!')
+    end
+
+    it 'must be logged in' do
+      user = FactoryBot.create(:user)
+      post1 = FactoryBot.create(:post)
+      like = FactoryBot.create(:like, user: user , post: post1)
+      like_params =  { post_id: post1.id, user: user.id, id: like.id}
       expect(Like.count).to eq(1)
 
-      expect { delete :destroy, params: { post_id: post1.id, user: user.id, id: like.id} }.to change(Like, :count).by(0)
-      expect(flash[:alert]).to eq('That is not your like to delete!')
+      delete :destroy, params: like_params
+
+      assert_redirected_to new_user_session_path
+      expect(Like.count).to eq(1)
     end
   end
 end
